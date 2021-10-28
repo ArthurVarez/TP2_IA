8d49274615f00e75607cac910d6866c64f39842a
Pepous <cyril.guirguis.06@gmail.com>
2021-10-27 21:54:40 -0400

    Ajout backtrack classique

--- src/Grille.py
+++ src/Grille.py
@@ -2,8 +2,6 @@ class Grille:
     def __init__(self,n=9):
         self.n=n
         self.creerMatriceAdjacenceGrapheContrainte()
-        self.grille=[[0 for i in range(n)] for j in range(n)]
-        self.afficherGrille()
     def memeLigne(self,coordA, coordB):
         return coordA[0]==coordB[0]
     def memeColonne(self,coordA, coordB):
@@ -37,8 +35,55 @@ class Grille:
         with open(path) as f:
             for line in f:
                 temp = [0 if x=="_" else int(x) for x in line.strip().split(" ") if x!=""]
-                grid.append(temp)
+                if(len(temp)!=0):
+                    grid.append(temp)
         self.grille = grid
+    def backtrackingSearch(self):
+        def grilleComplete(grille):
+            for i in range(n):
+                for j in range(n):
+                    if(grille[i][j]==0):
+                        return False
+            return True
+        def selectionnerVariableNonAssignee(grille):
+            for i in range(n):
+                for j in range(n):
+                    if(grille[i][j]==0):
+                        return [i,j]
+        def setValeursAutorisees(var, grille, csp):
+            index = var[0]*n+var[1]
+            possibilites = [1,2,3,4,5,6,7,8,9]
+            for index1 in range(n**2):
+                j=index1%n
+                i=(index1-j)//n
+                if(csp[index][index1]==1 and (grille[i][j] in possibilites)):
+                    possibilites.remove(grille[i][j])
+            return possibilites
 
+        def recursiveBacktracking(grille):
+            if(grilleComplete(grille)):
+                return grille
+            var = selectionnerVariableNonAssignee(grille)
+            valeursAutorisees = setValeursAutorisees(var, grille, csp)
+            orderDomainValues = [i for i in range(1,n+1)]
+            for value in orderDomainValues:
+                if(value in valeursAutorisees):
+                    grille[var[0]][var[1]]=value
+                    result = recursiveBacktracking(grille)
+                    if(result!=-1):
+                        return result
+                    grille[var[0]][var[1]]=0
+            return -1
+        grille=self.grille
+        csp = self.matriceAdjacenceGrapheContrainte
+        n=self.n
+        return recursiveBacktracking(grille)
+
+
+grille = Grille()
+path =r"C:\Users\Cyril\Documents\Cours UQAC\IA\TP2\git\TP2_IA\sudokus\sudoku1.txt"
+grille.ImportSudoku(path)
+grille.backtrackingSearch()
+grille.afficherGrille()
 
 
