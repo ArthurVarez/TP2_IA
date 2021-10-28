71025eff7ee13c065846c5fe668bf95263a45de3
Pepous <cyril.guirguis.06@gmail.com>
2021-10-28 00:45:07 -0400

    Ajout MRV

--- src/Grille.py
+++ src/Grille.py
@@ -1,3 +1,5 @@
+import time
+import math
 class Grille:
     def __init__(self,n=9):
         self.n=n
@@ -8,6 +10,7 @@ class Grille:
         return coordA[1]==coordB[1]
     def memeBloc(self,coordA, coordB):
         return (coordA[0]//3==coordB[0]//3 and coordA[1]//3==coordB[1]//3)
+
     def creerMatriceAdjacenceGrapheContrainte(self):
         n=self.n
         self.matriceAdjacenceGrapheContrainte = [[0 for i in range(n**2)] for i in range(n**2)]
@@ -21,6 +24,7 @@ class Grille:
                         index1=n*i1+j1
                         if((self.memeLigne(coordA,coordB) or self.memeColonne(coordA,coordB) or self.memeBloc(coordA,coordB)) and coordA!=coordB):
                            self.matriceAdjacenceGrapheContrainte[index][index1]=1
+
     def afficherGrille(self):
         n=self.n
         grille=self.grille
@@ -38,6 +42,7 @@ class Grille:
                 if(len(temp)!=0):
                     grid.append(temp)
         self.grille = grid
+
     def backtrackingSearch(self):
         def grilleComplete(grille):
             for i in range(n):
@@ -45,11 +50,13 @@ class Grille:
                     if(grille[i][j]==0):
                         return False
             return True
+
         def selectionnerVariableNonAssignee(grille):
             for i in range(n):
                 for j in range(n):
                     if(grille[i][j]==0):
                         return [i,j]
+
         def setValeursAutorisees(var, grille, csp):
             index = var[0]*n+var[1]
             possibilites = [1,2,3,4,5,6,7,8,9]
@@ -79,11 +86,71 @@ class Grille:
         n=self.n
         return recursiveBacktracking(grille)
 
+    def backtrackingSearchMRV(self):
+        def grilleComplete(grille):
+            for i in range(n):
+                for j in range(n):
+                    if(grille[i][j]==0):
+                        return False
+            return True
+
+        def setValeursAutorisees(var, grille, csp):
+            index = var[0]*n+var[1]
+            possibilites = [1,2,3,4,5,6,7,8,9]
+            for index1 in range(n**2):
+                j=index1%n
+                i=(index1-j)//n
+                if(csp[index][index1]==1 and (grille[i][j] in possibilites)):
+                    possibilites.remove(grille[i][j])
+            return possibilites
+
+        def selectionnerVariableNonAssignee(grille,csp):
+            nBValeurLegales=math.inf
+            var=[0,0]
+            for i in range(n):
+                for j in range(n):
+                    possibilites = setValeursAutorisees([i,j],grille,csp)
+                    if(grille[i][j]==0 and len(possibilites)<nBValeurLegales):
+                        nBValeurLegales = len(possibilites)
+                        var=[i,j]
+            return var
+
+        def recursiveBacktracking(grille,csp):
+            if(grilleComplete(grille)):
+                return grille
+            var = selectionnerVariableNonAssignee(grille,csp)
+            valeursAutorisees = setValeursAutorisees(var, grille, csp)
+            orderDomainValues = [i for i in range(1,n+1)]
+            for value in orderDomainValues:
+                if(value in valeursAutorisees):
+                    grille[var[0]][var[1]]=value
+                    result = recursiveBacktracking(grille,csp)
+                    if(result!=-1):
+                        return result
+                    grille[var[0]][var[1]]=0
+            return -1
+        grille=self.grille
+        csp = self.matriceAdjacenceGrapheContrainte
+        n=self.n
+        return recursiveBacktracking(grille,csp)
 
 grille = Grille()
-path =r"C:\Users\Cyril\Documents\Cours UQAC\IA\TP2\git\TP2_IA\sudokus\sudoku1.txt"
+path =r"C:\Users\Cyril\Documents\Cours UQAC\IA\TP2\git\TP2_IA\sudokus\sudoku2.txt"
 grille.ImportSudoku(path)
-grille.backtrackingSearch()
 grille.afficherGrille()
-
+print("\n")
+start= time.time()
+grille.backtrackingSearch()
+end = time.time()
+print(end-start)
+grille.afficherGrille()
+print("############")
+grille.ImportSudoku(path)
+grille.afficherGrille()
+print("\n")
+start = time.time()
+grille.backtrackingSearchMRV()
+end=time.time()
+print(end-start)
+grille.afficherGrille()
 
